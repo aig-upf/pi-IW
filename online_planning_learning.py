bd986aed81336b42ff88882c6c68def48f6ad78c
Miquel <miquel.junyentbarbany@gmail.com>
2019-03-08 20:37:28 +0100

    Added pi-IW with dynamic features. Guiding now planning with policy.

--- online_planning_learning.py
+++ online_planning_learning.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     from experience_replay import ExperienceReplay
     import tensorflow as tf
     import numpy as np
+    import timeit
 
     tf.enable_eager_execution()
     tf.enable_resource_variables()
@@ -16,8 +17,8 @@ if __name__ == "__main__":
     import gridenvs.examples #load simple envs
     from plan_step import gridenvs_BASIC_features
 
-    env_id = "GE_PathKeyDoor-v0"
-    max_tree_nodes = 30
+    env_id = "GE_MazeKeyDoor-v0"
+    tree_budget = 50
     discount_factor = 0.99
     cache_subtree = True
 
@@ -26,7 +27,17 @@ if __name__ == "__main__":
     learning_rate = 0.0007
     replay_capacity = 1000
     replay_min_transitions = 100
-    max_simulator_steps = 10000
+    max_simulator_steps = 1000000
+    regularization_factor = 0.001
+
+
+    clip_grad_norm = 40
+    rmsprop_decay = 0.99
+    rmsprop_epsilon = 0.1
+
+
+
+
 
     # Set random seed
     np.random.seed(seed)
@@ -35,18 +46,33 @@ if __name__ == "__main__":
     # Instead of env.step() and env.reset(), we'll use TreeActor helper class, which creates a tree and adds nodes to it
     env = gym.make(env_id)
     actor = TreeActor(env, observe_fn=gridenvs_BASIC_features)
-    planner = RolloutIW(branching_factor=env.action_space.n)
+    planner = RolloutIW(branching_factor=env.action_space.n, ignore_cached_nodes=True)
 
-    model = Mnih2013(num_logits=env.action_space.n)
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate)
-    learner = SupervisedPolicy(model, optimizer)
+    model = Mnih2013(num_logits=env.action_space.n, add_value=False)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate,
+                                          decay=rmsprop_decay,
+                                          epsilon=rmsprop_epsilon)
+    learner = SupervisedPolicy(model, optimizer, regularization_factor=regularization_factor, use_graph=True)
 
     experience_replay = ExperienceReplay(capacity=replay_capacity)
 
-    def planning_step(actor, planner, dataset):
+    def fill_nn(node):
+        x = tf.constant(np.expand_dims(node.data["obs"], axis=0).astype(np.float32))
+        logits, features = model(x, output_features=True)
+        node.data["probs"] = tf.nn.softmax(logits).numpy().flatten()
+        node.data["features"] = features.numpy().flatten()
+
+    def network_policy(node, branching_factor):
+        fill_nn(node)
+        return node.data["probs"]
+
+    def planning_step(actor, planner, dataset, policy):
+        nodes_before_planning = len(actor.tree)
+        budget_fn = lambda: len(actor.tree) - nodes_before_planning == tree_budget
         planner.plan(tree=actor.tree,
                      successor_fn=actor.generate_successor,
-                     stop_condition_fn=lambda: len(actor.tree) == max_tree_nodes)
+                     stop_condition_fn=budget_fn,
+                     policy_fn = policy)
 
         tree_policy = softmax_Q_tree_policy(actor.tree, env.action_space.n, discount_factor, temp=0)
         a = sample_pmf(tree_policy)
@@ -57,18 +83,24 @@ if __name__ == "__main__":
         return current_root_data["done"]
 
     # Initialize experience replay: run complete episodes until we exceed both batch_size and dataset_min_transitions
+    print("Initializing experience replay", flush=True)
     while len(experience_replay) < batch_size or len(experience_replay) < replay_min_transitions:
         episode_done = False
         actor.reset()
         while not episode_done:
-            episode_done = planning_step(actor, planner, dataset=experience_replay)
+            start = timeit.default_timer()
+            episode_done = planning_step(actor, planner, dataset=experience_replay, policy=network_policy)
+            print(actor.tree.root.data["s"], "\ttime:", timeit.default_timer()-start)
+            print("", flush=True)
 
     # Once initialized, interleave planning and learning steps
+    print("\nInterleaving planning and learning steps.", flush=True)
     tree = actor.reset()
     episode_transitions = ExperienceReplay()
     cnt = 0
     while actor.nodes_generated < max_simulator_steps:
-        episode_done = planning_step(actor, planner, dataset=episode_transitions)
+        start = timeit.default_timer()
+        episode_done = planning_step(actor, planner, dataset=episode_transitions, policy=network_policy)
 
         # Add transitions to dataset once the episode is over
         if episode_done:
@@ -82,3 +114,5 @@ if __name__ == "__main__":
                                      tf.constant(batch["target_policy"], dtype=tf.float32))
         cnt += 1
         print("Simulator steps:", actor.nodes_generated, "\tPlanning steps:", cnt, "\tLoss:", loss.numpy())
+        print(actor.tree.root.data["s"], "\ttime:", timeit.default_timer() - start)
+        print("", flush=True)
--- supervised_policy.py
+++ supervised_policy.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 
 class Mnih2013(tf.keras.models.Model):
-    def __init__(self, num_logits):
+    def __init__(self, num_logits, add_value=True):
         super(Mnih2013, self).__init__()
+        self.add_value = add_value
         self.conv1 = tf.keras.layers.Conv2D(filters=16,
                                             kernel_size=8,
                                             strides=4,
@@ -24,31 +25,48 @@ class Mnih2013(tf.keras.models.Model):
         self.logits = tf.keras.layers.Dense(units=num_logits,
                                             activation=None,
                                             name="policy_head")
-        self.value = tf.keras.layers.Dense(units=1,
-                                           activation=None,
-                                           name="value_head")
+        if self.add_value:
+            self.value = tf.keras.layers.Dense(units=1,
+                                               activation=None,
+                                               name="value_head")
 
-    def call(self, x):
+    def call(self, x, output_features=False):
         for l in self.hidden:
             x = l(x)
-        return self.logits(x), tf.reshape(self.value(x), (-1,)) # remove last dimension
+        logits = self.logits(x)
+        if self.add_value:
+            value = tf.reshape(self.value(x), (-1,))  # remove last dimension
+            if output_features:
+                return logits, value, x
+            else:
+                return logits, value
+
+        else:
+            if output_features:
+                return logits, x
+            else:
+                return logits
 
 
 class Learner:
-    def __init__(self, model, optimizer):
+    def __init__(self, model, optimizer, grad_proc_fn=None, use_graph=False):
         self.model = model
         self.optimizer = optimizer
+        self.grad_proc_fn = grad_proc_fn
+        if use_graph:
+            self.train_step = tf.contrib.eager.defun(self.train_step) # TODO: Update to tf.function by setting autograph to False
 
-    def train(self, get_batch_fn, steps, use_graph=False):
-        train_step_fn = tf.contrib.eager.defun(self.train_step, autograph=False) if use_graph else self.train_step # TODO: what if we defun more than once? (in this case by calling train several times)
+    def train(self, get_batch_fn, steps):
         for _ in range(steps):
             batch = get_batch_fn() # generates experience or gathers data from all actors
-            train_step_fn(*[tf.constant(tensor) for tensor in batch])
+            self.train_step(*[tf.constant(tensor) for tensor in batch])
 
     def train_step(self, *args, **kwargs):
         with tf.GradientTape() as gtape: # grads = self.optimizer.compute_gradients(batch)
             loss, partial_losses = self.loss(*args, **kwargs)
         grads = gtape.gradient(loss, self.model.variables)
+        if self.grad_proc_fn is not None:
+            grads = self.grad_proc_fn(grads)
         self.optimizer.apply_gradients(zip(grads, self.model.variables))
         return loss, partial_losses
 
@@ -71,12 +89,13 @@ def l2_regularization(variables, reduce_op=tf.reduce_sum):
 
 
 class SupervisedPolicy(Learner):
-    def __init__(self, model, optimizer, regularization_factor=0.0001):
-        super(SupervisedPolicy, self).__init__(model, optimizer)
+    def __init__(self, model, optimizer, regularization_factor=0.001, use_graph=False):
+        super(SupervisedPolicy, self).__init__(model, optimizer, use_graph=use_graph)
         self.regularization_factor = regularization_factor
 
     def loss(self, observations, target_policy):
-        logits, value = self.model(observations)
+        logits = self.model(observations)
+        assert type(logits) is not tuple
         cross_entropy = cross_entropy_loss(logits, target_policy, reduce_op=tf.reduce_mean)
         regularization = l2_regularization(self.model.variables, reduce_op=tf.reduce_sum)  # TODO: use reduce mean here?
         total_loss = cross_entropy + self.regularization_factor*regularization
